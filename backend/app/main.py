b5cd0de0a0245ba7d1c069a11c39cffc5620b427
Robbolo <167103133+Robbolo@users.noreply.github.com>
2026-07-31 08:49:56 +0100

    added recipe router to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import ingredients_router
+from app.routers import ingredients_router, recipe_router
 
 
 app = FastAPI(
@@ -17,7 +17,8 @@ app.add_middleware(
 )
 
 app.include_router(
-    ingredients_router
+    ingredients_router,
+    recipe_router
 )
 
 
--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,1 +1,2 @@
-from app.routers.ingredients import router as ingredients_router
+from app.routers.ingredients_router import router as ingredients_router
+from app.routers.recipe_router import router as recipe_router
