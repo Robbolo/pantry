8b82de70e159c88ef28b38b9013d0403f9207fcf
Robbolo <167103133+Robbolo@users.noreply.github.com>
2026-07-31 10:00:12 +0100

    added user id to dependencies for dev

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 
+DEV_USER_ID = 1
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
@@ -12,3 +13,6 @@ def get_db() -> Generator[Session, None, None]:
         yield db
     finally:
         db.close()
+
+def get_current_user_id() -> int:
+    return DEV_USER_ID
